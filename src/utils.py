112e08c95b95f77d0fbec2f1a747778565329b2e
TT-392 <TT-392@protonmail.com>
2022-09-02 23:15:47 +0200

    Add rotate_around()

---
+++ src/utils.py
@@ -0,0 +1,8 @@
+import math
+import numpy as np
+
+class utils:
+    def rotate_around(coord, pivot, angle):
+        r = math.hypot(coord[0] - pivot[0], coord[1] - pivot[1])
+        angle = np.angle(coord[0] - pivot[0] + (coord[1] - pivot[1])*1j) + np.deg2rad(angle)
+        return (math.cos(angle) * r + pivot[0], math.sin(angle) * r + pivot[1])
