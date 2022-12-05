dd3fd03c3dea7b97587389c02dfbe2cf6c4e6515
TT-392 <TT-392@protonmail.com>
2022-12-05 13:21:24 +0100

    Restore dropdown function

--- src/gui/elements/dropdown.py
+++ src/gui/elements/dropdown.py
@@ -28,16 +28,17 @@ class dropdown_control(Control):
         self.control.SetToolTip(varname)
 
     def set_value(self, value):
+        print(value)
         if value[:1] == "\"" and value[-1:] == "\"":
             isString = True
         else:
             isString = False
 
-        if not (isString and value[1:-1] in self.type.choices):
+        if not (isString and value[1:-1] in self.type.get_choices()):
             self.control.Append("-- mixed values --") #TODO: this only works once
-            self.control.SetSelection(len(self.type.choices))
+            self.control.SetSelection(len(self.type.get_choices()))
         else:
-            self.control.SetSelection(self.type.choices.index(value[1:-1]))
+            self.control.SetSelection(self.type.get_choices().index(value[1:-1]))
 
     def get_value(self):
         return "\"" + self.control.GetString(self.control.GetSelection()) + "\""
